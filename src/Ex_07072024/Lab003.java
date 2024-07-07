e4315dbf73049f793e581b4885898fee8e35303a
santhosh <santhoshstark12@gmail.com>
2024-07-07 19:21:05 +0530

    new program added

---
+++ src/Ex_07072024/Lab003.java
@@ -0,0 +1,11 @@
+package Ex_07072024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //single comment-comments are instructions for the code written
+        /**
+         * Multiple comments
+         * for documentation purpose
+         */
+    }
+}
